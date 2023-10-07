ada3eb115efd984e0ac850189b0a43cf450bca3e
ethanbli <ethanbli@tencent.com>
2023-10-07 19:34:16 +0800

    feat:add husky configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig, loadEnv, PluginOption } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import legacy from '@vitejs/plugin-legacy'
 import path from 'path'
@@ -7,7 +7,7 @@ const dirname = new URL('.', import.meta.url).pathname
 const resolve = (dir: string) => path.join(dirname, dir)
 
 const getPulgins = (command, appEnv) => {
-  const plugins: any[] = [react()]
+  const plugins: PluginOption[] = [react()]
   if (appEnv === '"prod"' && command === 'build') {
     plugins.push(
       legacy({
