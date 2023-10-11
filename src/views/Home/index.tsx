33728260d0229ee964221b1707e5e6a7fc0eafc8
ethanbli <ethanbli@tencent.com>
2023-10-11 10:33:37 +0800

    fix:add eslint

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -2,9 +2,11 @@ import http from '@/utils/http'
 import { useEffect } from 'react'
 import { Button } from 'antd'
 import _ from 'lodash-es'
+import { useTranslation } from 'react-i18next'
 
 export default () => {
   console.log('env mode:', process.env.APP_ENV)
+  const { t } = useTranslation()
   useEffect(() => {
     const params = {}
     // 获取首页数据
@@ -17,7 +19,7 @@ export default () => {
           _.add(1, 3)
         }}
       >
-        按钮
+        {t('按钮')}
       </Button>
     </>
   )
